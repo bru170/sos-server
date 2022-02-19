3684084e412289a82a11369f3e75d9ac44a4d2f8
bru170 <antoniabruno92@gmail.com>
2022-02-19 15:19:03 +0100

    added cors and first db query

--- index.js
+++ index.js
@@ -1,6 +1,19 @@
 const express = require("express")
 const app = express()
+const cors = require("cors")
 
-app.listen(3001, () => {
-  console.log("Listening on port 3001")
+var corsOptions = {
+  origin: "http://localhost:3001"
+}
+
+const db = require("./models")
+
+db.sequelize.sync().then(() => {
+  app.listen(3001, () => {
+    console.log("Listening on port 3001")
+  })
 })
+
+app.use(bodyParser.json())
+
+app.use(bodyParser.urlencoded({extended: true}))
