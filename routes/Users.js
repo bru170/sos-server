ab5b876072fbd05de60186fb55d43ff9696a0063
bru170 <antoniabruno92@gmail.com>
2022-05-10 14:43:28 +0300

    added access token (will chnage later)

--- routes/Users.js
+++ routes/Users.js
@@ -3,6 +3,8 @@ const router = express.Router()
 const {Users} = require("../models")
 const bcrypt = require("bcrypt")
 
+const {sign} = require("jsonwebtoken")
+
 router.post("/", async (request, response) => {
   const {username, fullName, email, password} = request.body
   bcrypt.hash(password, 10).then((hash) => {
@@ -28,6 +30,10 @@ router.post("/login", async (req, res) => {
       if (!match) {
         res.json({error: "Wrong Username And Password Combination"})
       } else {
+        const authToken = sign(
+          {username: user.username, id: user.id},
+          "PyLC59WCs6Ge9G9Mp6Yx0Qp6vowQ2HE8E4olyc2aZkipMTTVqlS1hfRtMJub4P2odnLEsqsDTEN63tW8aiOlxOCX40PhZU56jr5c"
+        )
         res.json("YOU LOGGED IN!!!")
       }
     })
